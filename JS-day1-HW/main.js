b7a28389bec257963fa984c5f431c313d796fbe8
Kristina Donaldson <donaldsonkristinar@yahoo.com>
2022-08-22 20:38:03 -0600

    Javascript Homework 1 -- Arrays and Functions

--- JS-day1-HW/main.js
+++ JS-day1-HW/main.js
@@ -1,12 +1,6 @@
 //==================Exercise #1 ==========//
-/*Write a function that takes in the string and the list of dog names, loops 
-through the list and checks that the current name is in the string passed in. 
-The output should be: "Matched dog_name" if name is in the string, if no matches 
-are present console.log "No Matches"
-*/
 let dog_string = "Hello Max, my name is Dog, and I have purple eyes!"
 let dog_names = ["Max","HAS","PuRple","dog"]
-// dog_names = ["this"]
 
 function findWords(str, arr){
     for(let i = 0; i < arr.length; i++){
@@ -20,10 +14,8 @@ function findWords(str, arr){
 
 console.log(findWords(dog_string, dog_names))
 
-//============Exercise #2 ============//
-/*Write a fucntion that takes in an array and removes every even index with a 
-splice, and replaces it with the string "even index" */
 
+//============Exercise #2 ============//
 let arr = ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
 
 function replaceEvens(arr){
@@ -36,6 +28,3 @@ function replaceEvens(arr){
 }
 
 console.log(replaceEvens(arr))
-
-//Expected output
-//Output arr == ["even index","Baseball","even index","Goku","even index","Rodger"]
